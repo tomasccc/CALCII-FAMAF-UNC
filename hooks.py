dcebf87a2fe3dbbefeddb19681fd889abe555122
405310-Caruso-Tomas <405310@tecnicatura.frc.utn.edu.ar>
2026-04-09 20:53:11 -0300

    fix: Remove trailing slash from base_url in configuration files

--- hooks.py
+++ hooks.py
@@ -27,7 +27,7 @@ def on_config(config):
             'name': 'github',
             'repo': 'tomasccc/CALCII-FAMAF-UNC',
             'branch': 'main',
-            'base_url': 'https://decap-proxy.tomycaruso.workers.dev/'
+            'base_url': 'https://decap-proxy.tomycaruso.workers.dev'
         },
         'publish_mode': 'editorial_workflow',
         'media_folder': 'docs/assets/comunidad',
